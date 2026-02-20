926b38c7d0a6efeae7983711c92fd6de50345e9c
BSenichak <bsenk77@gmail.com>
2026-02-20 11:34:15 +0200

    add frontend

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import mysql from "mysql2/promise";
+
+let pool = mysql.createPool({
+    host: "db",
+    user: "user",
+    password: "password",
+    database: "myapp_db",
+});
 
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Bun + Express + Docker Watch = ❤️");
+app.get("/",async (req, res) => {
+    let query = "show tables";
+    let result = await pool.query(query);
+  res.send("Bun + Express + Docker Watch = ❤️" + JSON.stringify(result[0]));
 });
 
 app.listen(port, () => {
